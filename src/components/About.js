5e2b34533258f19be0d1a89a21d7a4b8246b8f9f
WojciechK <wojciech.w.krupa@gmail.com>
2021-11-04 23:51:52 +0000

    Added about and projects info

--- src/components/About.js
+++ src/components/About.js
@@ -3,6 +3,8 @@ const About = () => {
         <div id="about" className="about" style={{ height: '200px' }}>
             <div className="container">
                 <h1>About</h1>
+                <h1>CALM, WARMTH, SMOOTHNESS - THIS COFFEE HOUSE IS LIKE A CUP OF CAPPUCCINO</h1>
+                <h3>This place is pure cosiness with a pinch of nice lounge atmosphere. Stove, rocking chair and bookshelf are waiting to make your stay even better. We will share the space for yours special occasions. </h3>
             </div>
         </div>
     )
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,7 +2,9 @@ const Projects = () => {
     return (
         <div id="projects" className="projects" style={{ height: '200px', backgroundColor: '#f00' }}>
             <div className="container">
-                <h1>Projects</h1>
+                <h2>COFFEEHOUSE it is host for many cyclical meetings, curses, artistic performances. </h2>
+                <p>Our Coffee Shop provides some events every week. We cooperate with positive people and sharing fantastic <a>groups</a> you can join! The #HUGAMUG comunity has <a>special meetings</a> including month of the author. </p>
+                <p>You can quickly check <a>the list of incomnig events</a>, be sure to find out benefits provided by our's shop membership. To become part of our comunity check the <a>join</a>section. </p>
             </div>
         </div>
     )
