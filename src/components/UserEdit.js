2a0d736edcc8f8a1dcfe0ec2bea62b65cd824994
WojciechK <wojciech.w.krupa@gmail.com>
2021-11-20 22:53:49 +0000

    creating edit user panel

--- src/components/UserEdit.js
+++ src/components/UserEdit.js
@@ -1,4 +1,27 @@
 const UserEdit = () => {
-    return (<h1>userEdit page</h1>)
+    return (<>
+           <div className="full-page flex-column flex-center default-background">
+            <div className="default-box-container">
+    <h1>userEdit page</h1>
+    {//birthday, hobby, favDrink,favCookie,userBioText,
+}
+                {/* {wrongPassword && <p className="message-error">{wrongPassword}</p>} */}
+                {/* <form onSubmit={onSubmit} className="flex-column"> */}
+                    <div className="flex-column flex-a-start flex-grow">
+                        {/* <label htmlFor="comment-form-user">Username or Email: </label>
+                        <input className="default-form w-80" id="comment-form-user" type="text" value={username} placeholder="Username" onChange={(e) => setUsername(e.target.value)}></input> */}
+                    </div>
+                    <div className="flex-column flex-a-start flex-grow">
+                        <label htmlFor="comment-form-rating">Password: </label>
+                        {/* <input className="default-form w-80" id="comment-form-rating" type="password" value={password} placeholder="Password" onChange={(e) => setPassword(e.target.value)}></input> */}
+                    </div>
+                    {/* <button className="button-react-icon button-block" type="submit"><FiSend className="react-icon" /></button> */}
+                {/* </form> */}
+                {/* <LoginForm /> */}
+                {/* <h3>If you dont have account, Sign in here: <Link to="/register"> <button className="button-react-icon"><FiFeather className="react-icon" /></button></Link></h3> */}
+            </div>
+        </div>
+        </>
+   )
 }
 export default UserEdit
