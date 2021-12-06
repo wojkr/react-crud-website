95ac1593c26c58378fbb51d5306215f883824d57
WojciechK <wojciech.w.krupa@gmail.com>
2021-12-06 23:41:35 +0000

    Changed styles

--- src/components/Join.js
+++ src/components/Join.js
@@ -1,47 +1,47 @@
 import { FiUserPlus } from "react-icons/fi"
 const Join = ({ isLoggedIn, setShowRegisterForm }) => {
     return (
-        <div id="join" className="full-page default-background">
-            <div className="h-100 flex-column flex-center">
-                <div className="w-50">
-                    <h1>Join us!</h1>
+        <div id="join" className="full-page default-background flex-column flex-center">
+            {/* <div className="h-100 flex-column flex-center"> */}
+            <div className="w-50 flex-column">
+                <h1>Join us!</h1>
+                <div className="default-box-container">
+                    <h2>What you will get</h2>
                     <div className="default-box-container">
-                        <h2>What you will get?</h2>
-                        <div className="default-box-container">
-                            <p>Being the member allows you to use these benefits:</p>
-                            <ul className="list-circle">
-                                <li>10% off for all beverages</li>
-                                <li>15% off for deals</li>
-                                <li>20% off for a favorite drink or a cookie</li>
-                                <li>25% off for favorite drink+cookie</li>
-                                <li>20% off to rent one of our room for your special occasieon</li>
-                                <li>free favorites for your birthday</li>
-                                <li>free favorites you and your friend who will join us</li>
-                                <li><strong>Spending time with amazing and inspiring people</strong></li>
-                            </ul>
-                        </div>
+                        <p>Being the member allows you to use these benefits:</p>
+                        <ul className="list-circle">
+                            <li>10% off for all beverages</li>
+                            <li>15% off for deals</li>
+                            <li>20% off for a favorite drink or a cookie</li>
+                            <li>25% off for favorite drink+cookie</li>
+                            <li>20% off to rent one of our room for your special occasieon</li>
+                            <li>free favorites for your birthday</li>
+                            <li>free favorites you and your friend who will join us</li>
+                            <li><strong>Spending time with amazing and inspiring people</strong></li>
+                        </ul>
                     </div>
-                    <div className="default-box-container">
-                        <h2>What you need to do to join?</h2>
-                        <div className="default-box-container">
-                            <p>We have got only {isLoggedIn ? 2 : 3} requirements:</p>
-                            <ul className="list-circle">
-                                {!isLoggedIn && <li>
-                                    Create your account on this website <i>(you can do it here: </i>
-                                    <button className="button-react-icon"
-                                        onClick={setShowRegisterForm}>
-                                        <FiUserPlus className="react-icon" />
-                                    </button>
-                                    <i>)</i>
-                                </li>}
-                                <li>Register at our reception</li>
-                                <li>Subscribe Membership <i>(Membership fee: £10/month)</i></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <h4>To become better verion of yourself stay with those who are getting improved</h4>
                 </div>
+                <div className="default-box-container">
+                    <h2>What you need to do to join</h2>
+                    <div className="default-box-container">
+                        <p>We have got only {isLoggedIn ? 2 : 3} requirements:</p>
+                        <ul className="list-circle">
+                            {!isLoggedIn && <li>
+                                Create your account on this website <i>(you can do it here: </i>
+                                <button className="button-react-icon"
+                                    onClick={setShowRegisterForm}>
+                                    <FiUserPlus className="react-icon" />
+                                </button>
+                                <i>)</i>
+                            </li>}
+                            <li>Register at our reception</li>
+                            <li>Subscribe Membership <i>(Membership fee: £10/month)</i></li>
+                        </ul>
+                    </div>
+                </div>
+                <h4>To become better verion of yourself stay with those who are getting improved</h4>
             </div>
+            {/* </div> */}
         </div>
     )
 }
--- src/components/User.js
+++ src/components/User.js
@@ -1,5 +1,4 @@
 import { FiEdit } from "react-icons/fi"
-// import {  FiUser } from "react-icons/fi"
 import LinkToGroup from "./LinkToGroup"
 import { useEffect, useLayoutEffect, useState } from "react"
 import { useParams, useNavigate, Link } from "react-router-dom"
@@ -29,45 +28,18 @@ const User = () => {
     useLayoutEffect(() => {
         let isSubscribed = true
         getData(`Users`, setUser, id, undefined, isSubscribed)
-        // const axios = require('axios')
-        // const options = {
-        //     headers: {
-        //         Accept: "*/*",
-        //         Authorization: "Bearer uf_test_admin_rbvqr4nd_9140bfa19f3e9af9bd1baef9d65d1935"
-        //     }
-        // }
-        // axios.get(`https://api.userfront.com/v0/users/` + id, options)
-        //     .then((response) => console.log(response.data))
-        //     .catch((err) => console.error(err))
         return () => isSubscribed = false
     }, [id])
 
-
-    // const editUserData = () => {
-    //     const axios = require('axios')
-    //     const payload = {
-    //         userId: 5,
-    //         email: "user@example.com",
-    //         username: "janedoe",
-    //         name: "Jane Doe",
-    //         image: "https://res.cloudinary.com/component/image/upload/avatars/avatar-16.png",
-    //         locked: false,
-    //         data: {
-    //             custom: "data"
-    //         }
-    //     }
-    //     axios.put("https://api.userfront.com/v0/users/" + 1, payload, options)
-    //         .then((response) => console.log(response.data))
-    //         .catch((err) => console.error(err));
-    // }
-
     return (
-        <div id="user" className="full-page default-background pt-10vh box-sizing-border-box">
+        <div id="user" className="full-page default-background box-sizing-border-box">
             {user.name ?
-                <div className="container flex-row flex-center">
-                    <div className="w-40 container-100 flex-column flex-center flex-a-start" style={{ borderRight: 'solid 1px #fff' }}>
-                        <div className="user-avatar-big-container">
-                            <img className="user-avatar-big" src={user.avatar || "https://res.cloudinary.com/b789b130931413a/image/upload/v1636730380/react-crud-website/avatar-default_gxvuj8.jpg"} alt={user.name}></img>
+                <div className="container flex-row flex-center flex-a-start">
+                    <div style={{ borderRight: 'solid 1px #fff' }}>
+                        <div>
+                            <div className="user-avatar-big-container">
+                                <img className="user-avatar-big" src={user.avatar || "https://res.cloudinary.com/b789b130931413a/image/upload/v1636730380/react-crud-website/avatar-default_gxvuj8.jpg"} alt={user.name}></img>
+                            </div>
                         </div>
                         <div className="flex-row border-light">
                             <h1>{user.name}</h1>
@@ -106,9 +78,9 @@ const User = () => {
                         <p className="border-light" >Favorite Drink: <Link to={("/products/" + user.favoriteDrinkId)}>{user.favoriteDrink}</Link></p>
                         <p>Favorite Cookies: <Link to={("/products/" + user.favoriteCookieId)}>{user.favoriteCookie}</Link></p>
                     </div>
-                    <div className="container-100 flex-column">
+                    <div className="flex-column">
                         {user.img ?
-                            <div className="user-image-container flex-grow">
+                            <div className="user-image-container">
                                 <img className="user-image" src={user.img} alt={user.name}></img>
                             </div>
                             :
